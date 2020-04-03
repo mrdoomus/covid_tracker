a94287900d3d16cbb0e8eaafff5b8640722b3ee0
mrdoomus <mrdoom.official@gmail.com>
2020-04-03 09:41:50 -0500

    Updates

---
+++ precache-manifest.e814cb6917101a70508b4a383c854a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faaf0019c028b16be80d5be648d57be",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "9509dd87beff639078e8",
+    "url": "/covid_tracker/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2c7c773b91411c65fd87",
+    "url": "/covid_tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9509dd87beff639078e8",
+    "url": "/covid_tracker/static/js/2.25a9ff25.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid_tracker/static/js/2.25a9ff25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7c773b91411c65fd87",
+    "url": "/covid_tracker/static/js/main.0030e34d.chunk.js"
+  },
+  {
+    "revision": "c0d319ff83ed94d58766",
+    "url": "/covid_tracker/static/js/runtime-main.a58a2dae.js"
+  }
+]);
